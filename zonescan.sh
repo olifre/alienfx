7f7edbdf870768c3883c9119271d6495baff8a0f
AlexCzar <caligo.spirituss@gmail.com>
2013-12-14 15:20:32 +0400

    update to work with alienfx M14xR2 and opensuse 13.1

--- zonescan.sh
+++ zonescan.sh
@@ -6,89 +6,89 @@
 # Version 3.
 
 echo "Lights out!"
-sudo ./alienfx -r
+sudo ./alienfx -P 521 -r
 echo "Press enter key for next zone"
 read
 echo "ZONE 01 (press enter)"
-sudo ./alienfx -r -v -c z01ffff
+sudo ./alienfx -P 521 -r -v -c z01ffff
 read
 echo "ZONE 02 (press enter)"
-sudo ./alienfx -r -v -c z02ffff
+sudo ./alienfx -P 521 -r -v -c z02ffff
 read
 echo "ZONE 03 (press enter)"
-sudo ./alienfx -r -v -c z03ffff
+sudo ./alienfx -P 521 -r -v -c z03ffff
 read
 echo "ZONE 04 (press enter)"
-sudo ./alienfx -r -v -c z04ffff
+sudo ./alienfx -P 521 -r -v -c z04ffff
 read
 echo "ZONE 05 (press enter)"
-sudo ./alienfx -r -v -c z05ffff
+sudo ./alienfx -P 521 -r -v -c z05ffff
 read
 echo "ZONE 06 (press enter)"
-sudo ./alienfx -r -v -c z06ffff
+sudo ./alienfx -P 521 -r -v -c z06ffff
 read
 echo "ZONE 07 (press enter)"
-sudo ./alienfx -r -v -c z07ffff
+sudo ./alienfx -P 521 -r -v -c z07ffff
 read
 echo "ZONE 08 (press enter)"
-sudo ./alienfx -r -v -c z08ffff
+sudo ./alienfx -P 521 -r -v -c z08ffff
 read
 echo "ZONE 09 (press enter)"
-sudo ./alienfx -r -v -c z09ffff
+sudo ./alienfx -P 521 -r -v -c z09ffff
 read
 echo "ZONE 10 (press enter)"
-sudo ./alienfx -r -v -c z10ffff
+sudo ./alienfx -P 521 -r -v -c z10ffff
 read
 echo "ZONE 11 (press enter)"
-sudo ./alienfx -r -v -c z11ffff
+sudo ./alienfx -P 521 -r -v -c z11ffff
 read
 echo "ZONE 12 (press enter)"
-sudo ./alienfx -r -v -c z12ffff
+sudo ./alienfx -P 521 -r -v -c z12ffff
 read
 echo "ZONE 13 (press enter)"
-sudo ./alienfx -r -v -c z13ffff
+sudo ./alienfx -P 521 -r -v -c z13ffff
 read
 echo "ZONE 14 (press enter)"
-sudo ./alienfx -r -v -c z14ffff
+sudo ./alienfx -P 521 -r -v -c z14ffff
 read
 echo "ZONE 15 (press enter)"
-sudo ./alienfx -r -v -c z15ffff
+sudo ./alienfx -P 521 -r -v -c z15ffff
 read
 echo "ZONE 16 (press enter)"
-sudo ./alienfx -r -v -c z16ffff
+sudo ./alienfx -P 521 -r -v -c z16ffff
 read
 echo "ZONE 17 (press enter)"
-sudo ./alienfx -r -v -c z17ffff
+sudo ./alienfx -P 521 -r -v -c z17ffff
 read
 echo "ZONE 18 (press enter)"
-sudo ./alienfx -r -v -c z18ffff
+sudo ./alienfx -P 521 -r -v -c z18ffff
 read
 echo "ZONE 19 (press enter)"
-sudo ./alienfx -r -v -c z19ffff
+sudo ./alienfx -P 521 -r -v -c z19ffff
 read
 echo "ZONE 20 (press enter)"
-sudo ./alienfx -r -v -c z20ffff
+sudo ./alienfx -P 521 -r -v -c z20ffff
 read
 echo "ZONE 21 (press enter)"
-sudo ./alienfx -r -v -c z21ffff
+sudo ./alienfx -P 521 -r -v -c z21ffff
 read
 echo "ZONE 22 (press enter)"
-sudo ./alienfx -r -v -c z22ffff
+sudo ./alienfx -P 521 -r -v -c z22ffff
 read
 echo "ZONE 23 (press enter)"
-sudo ./alienfx -r -v -c z23ffff
+sudo ./alienfx -P 521 -r -v -c z23ffff
 read
 echo "ZONE 24 (press enter)"
-sudo ./alienfx -r -v -c z24ffff
+sudo ./alienfx -P 521 -r -v -c z24ffff
 read
 echo "ZONE 25 (press enter)"
-sudo ./alienfx -r -v -c z25ffff
+sudo ./alienfx -P 521 -r -v -c z25ffff
 read
 echo "ZONE 26 (press enter)"
-sudo ./alienfx -r -v -c z26ffff
+sudo ./alienfx -P 521 -r -v -c z26ffff
 read
 echo "ZONE 27 (press enter)"
-sudo ./alienfx -r -v -c z27ffff
+sudo ./alienfx -P 521 -r -v -c z27ffff
 read
 echo "Lights out!"
-sudo ./alienfx -r
+sudo ./alienfx -P 521 -r
